76ea0a342fc37ea97b264e3ea3f562921f533875
hua-yan2025 <h.yan@leedstrinity.ac.uk>
2026-03-13 10:23:50 +0000

    Create demo.js

---
+++ demo.js
@@ -0,0 +1,34 @@
+// Task 1: JSON Array
+{
+    document.addEventListener("DOMContentLoaded", function() {
+    // fetch() GET
+    fetch("https://huayan.pythonanywhere.com/animals")
+        .then(response => response.json())
+        .then(data => {
+            const list = document.getElementById("animalList");
+            data.forEach(fruit => {
+                list.innerHTML += "<li>" + fruit + "</li>";
+            });
+        });
+});
+}
+
+// Task 2: JSON Object
+function getStudentInfo(){
+    const inputName = document.getElementById("inputName").value;
+    // fetch() POST
+    fetch("https://huayan.pythonanywhere.com/studentInfo",{
+        method: "POST",
+        headers:{"Content-Type":"application/json"},
+        body:JSON.stringify({name:inputName})
+    })
+    .then(response => response.json())
+    .then(data =>{
+        document.getElementById("photo").src = data.photo;
+            document.getElementById("name").textContent = data.name;
+            document.getElementById("age").textContent = data.age;
+            document.getElementById("course").textContent = data.course;
+            document.getElementById("level").textContent = data.level;
+            document.getElementById("department").textContent = data.department;
+    });
+}
